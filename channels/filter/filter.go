1a40baa4f5e2f07e8dfb1558194bd454ee293206
Mark Partola <partolamark@yandex.ru>
2025-04-03 22:49:22 +0800

    feat: add filter channels pattern

---
+++ channels/filter/filter.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		defer close(ch)
+		for i := range 10 {
+			ch <- i
+		}
+	}()
+
+	for v := range filter(ch, func(v int) bool {
+		return v%2 == 0
+	}) {
+		fmt.Println(v)
+	}
+}
+
+func filter(ch <-chan int, f func(int) bool) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for v := range ch {
+			if f(v) {
+				out <- v
+			}
+		}
+	}()
+
+	return out
+}
